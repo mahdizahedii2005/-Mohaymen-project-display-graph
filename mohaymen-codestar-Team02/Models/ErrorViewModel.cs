2cb05259c1a0c95ee378c66378d4e66af74f6bb5
Ftm-Sayadzadeh <ftmdkf@gmail.com>
2024-08-11 17:20:16 +0430

    init : initialize back-end project

---
+++ mohaymen-codestar-Team02/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace mohaymen_codestar_Team02.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
