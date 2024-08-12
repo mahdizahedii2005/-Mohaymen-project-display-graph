ce1189df3ab70198fcee391e2e3895148c2cf4b9
Mahdizahedi2005 <mahdi.zahedi.mahdi@gmail.com>
2024-08-12 17:12:51 +0330

    fix : fix the model to many to many

--- mohaymen-codestar-Team02/Models/Role.cs
+++ mohaymen-codestar-Team02/Models/Role.cs
@@ -3,8 +3,8 @@ namespace mohaymen_codestar_Team02.Models;
 public class Role
 {
     public long RoleId { get; set; } 
-    public string Name { get; set; }
-    public User User { get; set; }
     public long UserId { get; set; }
     public RoleType RoleType { get; set; }
+    public HashSet<UserRole> UserRoles { get; set; }
+
 }
---
+++ mohaymen-codestar-Team02/Models/UserRole.cs
@@ -0,0 +1,10 @@
+using mohaymen_codestar_Team02.Models;
+
+public class UserRole
+{
+    public long UserId { get; set; }
+    public User User { get; set; }
+
+    public long RoleId { get; set; }
+    public Role Role { get; set; }
+}
