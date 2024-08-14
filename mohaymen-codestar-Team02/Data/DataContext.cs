b56d5238111036908b2d2f028dff9a577bb70516
HanaNrvtn <hanaNrvtn@gmail.com>
2024-08-13 23:51:41 -0700

    refactor : DataContext modified

--- mohaymen-codestar-Team02/Data/DataContext.cs
+++ mohaymen-codestar-Team02/Data/DataContext.cs
@@ -11,28 +11,6 @@ public class DataContext : DbContext
     public DbSet<Role> Roles { get; set; }
     public DbSet<UserRole> UserRoles { get; set; }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<UserRole>()
-            .HasKey(ur => new { ur.UserId, ur.RoleId });
-
-        modelBuilder.Entity<UserRole>()
-            .HasOne(ur => ur.User)
-            .WithMany(u => u.UserRoles)
-            .HasForeignKey(ur => ur.UserId);
-
-        modelBuilder.Entity<UserRole>()
-            .HasOne(ur => ur.Role)
-            .WithMany(r => r.UserRoles)
-            .HasForeignKey(ur => ur.RoleId);
-
-        modelBuilder.Entity<Role>()
-            .Property(ur => ur.RoleType)
-            .HasConversion<int>(); 
-
-        base.OnModelCreating(modelBuilder);
-    } 
-    
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseLazyLoadingProxies();
