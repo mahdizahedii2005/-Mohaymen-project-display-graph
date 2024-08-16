c4928f31d61456b2712529979120c4151460c6eb
Alireza Jafari <alirezaja1384@gmail.com>
2024-08-17 02:58:11 +0330

    feat(runtime migration): EnsureCreated()

--- mohaymen-codestar-Team02/Program.cs
+++ mohaymen-codestar-Team02/Program.cs
@@ -36,6 +36,8 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
 
     var context = services.GetRequiredService<DataContext>();
+    context.Database.EnsureCreated();
+    
     if (context.Database.GetPendingMigrations().Any())
     {
         context.Database.Migrate();
