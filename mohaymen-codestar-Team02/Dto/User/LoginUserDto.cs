f59edf220ae085e2c4681e4013de81d369bd49c3
Ftm-Sayadzadeh <ftmdkf@gmail.com>
2024-08-16 20:26:46 +0430

    refactor : add validation attributes to DTOs

--- mohaymen-codestar-Team02/Dto/User/LoginUserDto.cs
+++ mohaymen-codestar-Team02/Dto/User/LoginUserDto.cs
@@ -1,7 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace mohaymen_codestar_Team02.Dto.UserDtos;
 
 public class LoginUserDto
 {
-    public string Username { get; set; }
-    public string Password { get; set; }
+    [Required] public string Username { get; set; }
+    [Required] public string Password { get; set; }
 }
