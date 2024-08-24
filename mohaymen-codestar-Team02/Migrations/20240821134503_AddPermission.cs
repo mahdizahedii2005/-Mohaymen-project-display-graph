2f14391510b4b1d151b0997616dc71a347ec2936
Mahdizahedi2005 <mahdi.zahedi.mahdi@gmail.com>
2024-08-24 09:45:17 +0330

    build : add migration

---
+++ mohaymen-codestar-Team02/Migrations/20240821134503_AddPermission.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mohaymen_codestar_Team02.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPermission : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int[]>(
+                name: "Permissions",
+                table: "Roles",
+                type: "integer[]",
+                nullable: false,
+                defaultValue: new int[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Permissions",
+                table: "Roles");
+        }
+    }
+}
