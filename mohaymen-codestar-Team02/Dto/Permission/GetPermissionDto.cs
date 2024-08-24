6fa9ca52c15bb15ebea66151932e1a2da26b50d0
Ftm-Sayadzadeh <ftmdkf@gmail.com>
2024-08-24 17:11:10 +0430

    fix: change dto field and add new dto

---
+++ mohaymen-codestar-Team02/Dto/Permission/GetPermissionDto.cs
@@ -0,0 +1,8 @@
+namespace mohaymen_codestar_Team02.Dto.Permission;
+using mohaymen_codestar_Team02.Models;
+
+public class GetPermissionDto
+{
+    public List<Permission> Permissions { get; init; }
+
+}
