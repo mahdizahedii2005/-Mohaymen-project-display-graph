d2cc07ac387ae78789cb3d4802c90dfa3dd51d01
Ftm-Sayadzadeh <ftmdkf@gmail.com>
2024-08-24 18:05:39 +0430

    refactor: just moved models in dir

--- mohaymen-codestar-Team02/Models/DataGraph/DataGroup.cs
+++ mohaymen-codestar-Team02/Models/DataGraph/DataGroup.cs
@@ -1,6 +1,5 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Diagnostics;
 using mohaymen_codestar_Team02.Models.EdgeEAV;
 using mohaymen_codestar_Team02.Models.VertexEAV;
 
