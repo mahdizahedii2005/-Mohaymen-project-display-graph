1f70a00dd3e43fc47293e9511be72fabfb346b01
Alireza Jafari <alirezaja1384@gmail.com>
2024-08-24 11:58:51 +0000

    feat: Bundled migration

---
+++ mohaymen-codestar-Team02/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -e
+
+BASE_DIR=$(dirname "$0")
+
+migrate() {
+    echo "Migrating database"
+    "${BASE_DIR}/dbmigrate" $@
+}
+
+run() {
+    echo "Running application"
+    dotnet "$BASE_DIR/mohaymen-codestar-Team02.dll"
+}
+
+case "$1" in
+migrate)
+    additional_args=${@:2}
+    migrate $additional_args
+    ;;
+run)
+    run
+    ;;
+*)
+    migrate
+    run
+    ;;
+esac
