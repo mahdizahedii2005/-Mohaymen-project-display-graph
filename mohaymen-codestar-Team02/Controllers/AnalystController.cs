7b3cb63d3b44ee56111b6c94a6ef8d7c23ec1ef1
Mahdizahedi2005 <mahdi.zahedi.mahdi@gmail.com>
2024-08-27 10:40:55 +0330

    feat: add dep

--- mohaymen-codestar-Team02/Controllers/AnalystController.cs
+++ mohaymen-codestar-Team02/Controllers/AnalystController.cs
@@ -1,10 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using mohaymen_codestar_Team02.Dto;
+using mohaymen_codestar_Team02.Services.AnalystService;
 
 namespace mohaymen_codestar_Team02.Controllers;
 
 public class AnalystController : ControllerBase
 {
+    private readonly IAnalystService AnalystService;
+
+    public AnalystController(IAnalystService analystService)
+    {
+        AnalystService = analystService;
+    }
+
     [HttpGet("Analyst")]
     public Task<IActionResult> SearchGraph([FromQuery] GraphQueryInfoDto graphQueryInfoDto, [FromQuery] Dictionary<string, string> vertexAttributeValues)
     {
